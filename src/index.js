37f98f3d15e1082baa10eceae1d3545df76df818
noellegisewhite <noellegisewhite@gmail.com>
2022-11-18 19:45:38 -0500

    file name bug fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Home from './components/Home';
-import Ground from './components/Ground';
+import Home from './Components/Home';
+import Ground from './Components/Ground';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
